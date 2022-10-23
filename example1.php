130645d034a3e4aabc6a4b8ce25cdfd2aa09c955
zjxxyy <zjxkl1314@163.com>
2022-10-23 22:20:24 +0800

    update
    first version

---
+++ example1.php
@@ -0,0 +1,48 @@
+<?php
+#phpinfo();
+//$a = 6;
+//$b = 7;
+//$c = $a+$b;
+//print $c;
+//print "\n\r";
+//echo $c;
+//$shiliu = 0x6ff;
+//print "\n\r";
+//echo $shiliu;
+//$zhifu = '曾经有操蛋的爱情摆在我面前，我珍惜了。当我得到的时候才感到后悔莫及。如果非要在这段爱情前面加上一段三个字，我愿意说三个字：滚犊子';
+//
+////你可以放XAMPP指定的目录下，新建一个文件叫str.php。然后访问一下http://127.0.0.1/str.php试试。会不会显示这句话。
+//
+//echo $zhifu;
+//$fudian = 121121.3133;
+//echo $fudian;
+//
+//$f1 = 0.8881;
+//var_dump($f1);
+//$pao = true;
+//
+//if($pao) {
+//   echo '我愿意去死';
+//   echo "凌志玲，我爱死你了";
+//
+//} else {
+//    echo "我不愿意去死";
+//}
+//$one =10;
+//$two = false;
+//$three = 0;
+//$four = null;
+//$result = isset($one,$two,$three,$four);
+//var_dump($result);
+//$jia = false;
+//$result = isset($jia);
+//var_dump($result);
+//$shu = array(1,2,3);
+//var_dump($shu);
+#$imgname = "https://pics6.baidu.com/feed/0823dd54564e92586c93c6f30f140e50cdbf4ea3.jpeg@f_auto?token=6564dc83b596d74377d5a3554898f110";
+#echo '<img src=$imgname width="300" height="250" />';
+#echo "<img src='https://pics6.baidu.com/feed/0823dd54564e92586c93c6f30f140e50cdbf4ea3.jpeg' />";
+echo "<img src='1.jpeg' />";
+echo "<br />";      //换行
+#imagejpeg($imgname);
+?>
